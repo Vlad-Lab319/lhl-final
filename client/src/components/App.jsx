bbd2d1c4a3e890459e5d89f1ea3d3e0733749b8b
Emily <emilyabigailwaters@gmail.com>
2022-02-10 11:26:00 -0800

    Update filenames to jsx, Restructure project folders, Add server logic, Add proxy for client, Install normalize.css, Install classnames

---
+++ client/src/components/App.jsx
@@ -0,0 +1,11 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">App Goes Here</header>
+    </div>
+  );
+}
+
+export default App;
