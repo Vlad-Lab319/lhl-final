c29cb04583168cb163541100ed552f0241cd5f2e
Emily <emilyabigailwaters@gmail.com>
2022-02-12 21:46:39 -0800

    Add UseApplicationData for managing state, server requests and websocket

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,36 +1,49 @@
+// Helpers
+import {
+  getChannelsForRoom,
+  getMessagesForChannel,
+} from "../helpers/selectors";
+// State
+import useApplicationData from "../hooks/useApplicationData";
+// Styles - scss
 import "../styles/App.scss";
+// Components
 import ChannelList from "./ChannelList";
 import ChatInput from "./ChatInput";
 import FriendList from "./FriendList";
 import Header from "./Header";
 import MessageList from "./MessageList";
 import RoomList from "./RoomList";
-import WebRtc from "./WebRTC";
 
 const App = () => {
+  const { state, setChannel, setRoom, setUser } = useApplicationData();
+  const channelList = getChannelsForRoom(state.room, state);
+  const messageList = getMessagesForChannel(state.channel, state);
+  // const user = setUser;
   return (
     <main className="layout">
       <header className="header">
+        {/* TODO: Login in the header */}
         <Header />
       </header>
       <div className="main-container">
         <div className="sidebar sidebar--rooms">
-          <RoomList />
+          <RoomList setRoom={setRoom} />
         </div>
 
         <div className="sidebar sidebar--channels">
-          <ChannelList />
+          <ChannelList setChannel={setChannel} channelList={channelList} />
         </div>
         <div className="messages">
-          <MessageList />
+          <MessageList messageList={messageList} />
           <ChatInput />
         </div>
         <div className="sidebar sidebar--friends">
           <FriendList />
         </div>
-        <div className="webrtc">
+        {/* <div className="webrtc">
           <WebRtc />
-        </div>
+        </div> */}
       </div>
     </main>
   );
