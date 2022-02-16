2855ce982df84deb93681b59100eaa4141b43c37
harisrizwan1 <harisrizwan985@gmail.com>
2022-02-16 14:28:50 -0600

    modify database messages table

--- server/db/schema/04_messages.sql
+++ server/db/schema/04_messages.sql
@@ -3,6 +3,7 @@ CREATE TABLE messages (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   channel_id INTEGER REFERENCES channels(id) ON DELETE CASCADE,
+  recipient_id INTEGER REFERENCES users(id) ON DELETE CASCADE DEFAULT null ,
   message TEXT NOT NULL,
   created_at TIMESTAMP DEFAULT NOW()
 );
