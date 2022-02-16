805edc3a32ac0a8e9c4d40fa2db6412eb7912648
vlad-lab319 <solo15mag@gmail.com>
2022-02-16 17:08:38 -0500

    Add create room button, popup dialog and dummy function for save to db

--- client/src/components/RoomList.jsx
+++ client/src/components/RoomList.jsx
@@ -1,8 +1,37 @@
+import { useState } from "react";
 import "../styles/RoomList.scss";
 import RoomListItem from "./RoomListItem";
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+
+//mui material 
+import Button from '@mui/material/Button';
+import Input from '@mui/material/Input';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+
+
 
 const RoomList = (props) => {
   const { roomList, setRoom, value } = props;
+
+  const [open, setOpen] = useState(false);
+  const [newRoomName, setNewRoomName] = useState("");
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setNewRoomName("");
+  };
+
+
+
   const rooms = roomList.map((room) => {
     return (
       <RoomListItem
@@ -13,10 +42,48 @@ const RoomList = (props) => {
         setRoom={() => setRoom(room)}
         selected={room.id === value.id}
       />
+
     );
   });
 
-  return <div className="sidebar sidebar--rooms">{rooms}</div>;
+  function createRoom() {
+    console.log(`Create room ${newRoomName}`);
+    handleClose();
+  }
+
+  return (
+    <>
+      <div className="sidebar sidebar--rooms">
+        {rooms}
+        <div className="room-container">
+          <AddCircleIcon onClick={handleClickOpen} />
+          <Dialog open={open} onClose={handleClose}>
+            <DialogTitle>Create new room</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                Create your amazing new room!
+              </DialogContentText>
+              <Input
+                autoFocus
+                margin="dense"
+                type="text"
+                fullWidth
+                variant="standard"
+                id="room_id"
+                value={newRoomName}
+                onChange={(event) => setNewRoomName(event.target.value)}
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose}>Cancel</Button>
+              <Button onClick={createRoom}>Create</Button>
+            </DialogActions>
+          </Dialog>
+
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default RoomList;
