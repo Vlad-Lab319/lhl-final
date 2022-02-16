40f0ff03676b4437aff25ce650d384c87e15ac35
Emily <emilyabigailwaters@gmail.com>
2022-02-15 20:25:31 -0800

    Create helpers.js, add avatar default functionality in MessageListItem. If a user doesn't have an avatar associated with their account then one is auto gen'd for them

--- client/src/components/MessageListItem.jsx
+++ client/src/components/MessageListItem.jsx
@@ -1,6 +1,8 @@
 import classNames from "classnames";
 import "../styles/MessageListItem.scss";
 import ReactTimeAgo from 'react-time-ago'
+import Avatar from '@mui/material/Avatar';
+import stringAvatar from "../helpers/helpers";
 
 
 // TODO: MessageListItem needs the proper user object
@@ -13,12 +15,11 @@ const MessageListItem = (props) => {
 
   return (
     <ul className={messageClass}>
-
+      {avatar ?
       <img src={avatar} alt="" className="message-icon"/>
-
+    : <Avatar {...stringAvatar(name)} />}
         <div className="message-content">
         <div className="message-header">
-
         <span className="message-user">{name}</span>
         <ReactTimeAgo className="message-time" date={time} locale="en-US"/>
       </div>
