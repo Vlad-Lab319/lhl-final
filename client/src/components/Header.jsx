57322d5c55f0d67a42de7a92a4391d8048c8741e
Emily <emilyabigailwaters@gmail.com>
2022-02-16 11:10:23 -0800

    Change hover style for room icons

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,14 +4,15 @@ import SettingsIcon from '@mui/icons-material/Settings';
 // TODO: Header needs actual interactive icons rather than the placeholder text here, register/login/logout needs to be implemented (Probably create a new component that get rendered by the Header and handles that)
 
 const Header = (props) => {
-  const {user} = props;
+  const { user } = props;
+  console.log(user);
   return (
     <div className="container">
       <div className="user-options user-options--logo">LOGO</div>
       <div className="user-options">
         {user
           ? <>
-          <SettingsIcon className="user-options user-options--gear"/><img className="user-options user-options--avatar" src={user.avatar} alt="" />
+          <SettingsIcon className="user-options user-options--gear"/><img className="user-options user-options--avatar" src={user.avatar} alt={`Logged in as ${user.name}`} />
           </>
           :
           <AccountCircleIcon className="user-options user-options--avatar" />
