af96de9972d150d4cc047a9a98afd10ab150f842
Emily <emilyabigailwaters@gmail.com>
2022-02-17 17:27:11 -0800

    Fix bug where add channel was showing when no room is selected, Fix ugly button behaviour

--- client/src/components/Channel/ChannelList.jsx
+++ client/src/components/Channel/ChannelList.jsx
@@ -1,4 +1,5 @@
 //mui material
+import AddBoxIcon from "@mui/icons-material/AddBox";
 import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
@@ -6,11 +7,10 @@ import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 import Input from "@mui/material/Input";
-import AddBoxIcon from "@mui/icons-material/AddBox";
 import { useState } from "react";
 import "./ChannelList.scss";
 import ChannelListItem from "./ChannelListItem";
-import { IconButton } from "@mui/material";
+import "./ChannelListItem.scss";
 
 const ChannelList = (props) => {
   const { channelList, setChannel, value, room, createChannel } = props;
@@ -52,9 +52,12 @@ const ChannelList = (props) => {
 
   const addButton = (
     <>
-      <IconButton color="inherit">
-        <AddBoxIcon onClick={handleClickOpen} />
-      </IconButton>
+      <div className="channel-item" onClick={handleClickOpen}>
+        <div className="channel-icon">
+          <AddBoxIcon />
+        </div>
+        <span>Create a channel</span>
+      </div>
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle>Create new channel</DialogTitle>
         <DialogContent>
@@ -80,9 +83,9 @@ const ChannelList = (props) => {
 
   return (
     <div className="sidebar sidebar--channels">
-      <h3 className="channel-title">{room.name && `${room.name}`}</h3>
-      {channels}
-      {addButton}
+      {room.name && <h3 className="channel-title">{room.name}</h3>}
+      {room.name && channels}
+      {room.name && addButton}
     </div>
   );
 };
