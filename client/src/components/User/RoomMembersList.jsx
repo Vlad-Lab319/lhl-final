b6486c349b43ecbdd5e57760f52b83ae4104988e
Emily <emilyabigailwaters@gmail.com>
2022-02-18 11:14:12 -0800

    Add logic to roomMemberList, Add getUsersForRoom in selectors

--- client/src/components/User/RoomMembersList.jsx
+++ client/src/components/User/RoomMembersList.jsx
@@ -1,14 +1,17 @@
-// import RoomMembersListItem from './RoomMembersListItem'
-
 // TODO: RoomMembersList needs the users present in the room that the user is currently in
-
 import "./RoomMembersList.scss";
+import RoomMembersListItem from "./RoomMembersListItem";
+
 const RoomMembersList = (props) => {
   const { memberList } = props;
+
+  const members = memberList.map((user) => {
+    <RoomMembersListItem />;
+  });
   return (
     <>
-      <h3 className="members-title">Online -- #ofActive</h3>
-      {memberList}
+      <h3 className="members-title">Online -- {members.length}</h3>
+      {members}
     </>
   );
 };
